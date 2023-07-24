f7177c766b51f9a672d10eb1a770239783257128
husada20 <husadahutasoit@gmail.com>
2023-07-24 23:20:57 +0700

    upgrade major

--- app.go
+++ app.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	basic_package_module "github.com/husadahts/basic-package-module"
+	basic_package_module "github.com/husadahts/basic-package-module/v2"
 )
 
 func main() {
-	fmt.Println(basic_package_module.SayHello())
+	fmt.Println(basic_package_module.SayHello(" v2.0.0"))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/husadahts/app-say-hello
 
 go 1.20
 
-require github.com/husadahts/basic-package-module v0.0.0-20230724152032-ce0c4c720c32 // indirect
+require github.com/husadahts/basic-package-module/v2 v2.0.0
