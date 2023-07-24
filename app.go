5df3a7ec663a491d43262392ab83fca0787f6291
husada20 <husadahutasoit@gmail.com>
2023-07-24 22:44:09 +0700

    get module basic-package

---
+++ app.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	basic_package_module "github.com/husadahts/basic-package-module"
+)
+
+func main() {
+	fmt.Println(basic_package_module.SayHello())
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/husadahts/app-say-hello
+
+go 1.20
+
+require github.com/husadahts/basic-package-module v0.0.0-20230724152032-ce0c4c720c32 // indirect
